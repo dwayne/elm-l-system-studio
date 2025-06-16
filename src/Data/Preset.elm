95b1a276da7d1646a5546b2ab2fb9960c05314e2
Dwayne Crooks <me@dwaynecrooks.com>
2025-06-16 06:57:24 -0400

    Add more presets

--- src/Data/Preset.elm
+++ src/Data/Preset.elm
@@ -54,6 +54,34 @@ presets =
       , name = "Bush 5"
       , settings = Settings.bush5
       }
+    , { id = "sticks"
+      , name = "Sticks"
+      , settings = Settings.sticks
+      }
+    , { id = "algae1"
+      , name = "Algae 1"
+      , settings = Settings.algae1
+      }
+    , { id = "algae2"
+      , name = "Algae 2"
+      , settings = Settings.algae2
+      }
+    , { id = "weed"
+      , name = "Weed"
+      , settings = Settings.weed
+      }
+    , { id = "triangle"
+      , name = "Triangle"
+      , settings = Settings.triangle
+      }
+    , { id = "quadraticGosper"
+      , name = "Quadratic Gosper"
+      , settings = Settings.quadraticGosper
+      }
+    , { id = "squareSierpinski"
+      , name = "Square Sierpinski"
+      , settings = Settings.squareSierpinski
+      }
     , { id = "crystal"
       , name = "Crystal"
       , settings = Settings.crystal
@@ -62,13 +90,95 @@ presets =
       , name = "Peano Curve"
       , settings = Settings.peanoCurve
       }
-    , { id = "quadraticSnowflake"
-      , name = "Quadratic Snowflake"
-      , settings = Settings.quadraticSnowflake
+    , { id = "quadraticSnowflake1"
+      , name = "Quadratic Snowflake 1"
+      , settings = Settings.quadraticSnowflake1
+      }
+    , { id = "quadraticSnowflake2"
+      , name = "Quadratic Snowflake 2"
+      , settings = Settings.quadraticSnowflake2
+      }
+    , { id = "quadraticKochIsland1"
+      , name = "Quadratic Koch Island 1"
+      , settings = Settings.quadraticKochIsland1
+      }
+    , { id = "quadraticKochIsland2"
+      , name = "Quadratic Koch Island 2"
+      , settings = Settings.quadraticKochIsland2
+      }
+    , { id = "quadraticKochIsland3"
+      , name = "Quadratic Koch Island 3"
+      , settings = Settings.quadraticKochIsland3
+      }
+    , { id = "board"
+      , name = "Board"
+      , settings = Settings.board
+      }
+    , { id = "hilbert"
+      , name = "Hilbert"
+      , settings = Settings.hilbert
+      }
+    , { id = "sierpinskiArrowhead"
+      , name = "Sierpinski Arrowhead"
+      , settings = Settings.sierpinskiArrowhead
       }
     , { id = "vonKochSnowflake"
       , name = "von Koch Snowflake"
       , settings = Settings.vonKochSnowflake
+      }
+    , { id = "cross1"
+      , name = "Cross 1"
+      , settings = Settings.cross1
+      }
+    , { id = "cross2"
+      , name = "Cross 2"
+      , settings = Settings.cross2
+      }
+    , { id = "pentaplexity"
+      , name = "Pentaplexity"
+      , settings = Settings.pentaplexity
+      }
+    , { id = "rings"
+      , name = "Rings"
+      , settings = Settings.rings
+      }
+    , { id = "dragonCurve"
+      , name = "Dragon Curve"
+      , settings = Settings.dragonCurve
+      }
+    , { id = "hexagonalGosper"
+      , name = "Hexagonal Gosper"
+      , settings = Settings.hexagonalGosper
+      }
+    , { id = "levyCurve"
+      , name = "Levy Curve"
+      , settings = Settings.levyCurve
+      }
+    , { id = "classicSierpinskiCurve"
+      , name = "Classic Sierpinski Curve"
+      , settings = Settings.classicSierpinskiCurve
+      }
+    , { id = "krishnaAnklets"
+      , name = "Krishna Anklets"
+      , settings = Settings.krishnaAnklets
+      }
+
+    --
+    -- TODO: Uncomment after openPolygon and closePolygon has been implemented.
+    --
+    --, { id = "mangoLeaf"
+    --  , name = "Mango Leaf"
+    --  , settings = Settings.mangoLeaf
+    --  }
+    --
+    --, { id = "snakeKolam"
+    --  , name = "Snake Kolam"
+    --  , settings = Settings.snakeKolam
+    --  }
+    --
+    , { id = "kolam"
+      , name = "Kolam"
+      , settings = Settings.kolam
       }
     ]
 
--- src/Data/Settings.elm
+++ src/Data/Settings.elm
@@ -1,21 +1,47 @@
 module Data.Settings exposing
     ( Settings
+    , algae1
+    , algae2
+    , board
     , bush1
     , bush2
     , bush3
     , bush4
     , bush5
+    , classicSierpinskiCurve
+    , cross1
+    , cross2
     , crystal
     , default
+    , dragonCurve
+    , hexagonalGosper
+    , hilbert
     , kochCurve
+    , kolam
+    , krishnaAnklets
     , leaf
+    , levyCurve
+    , mangoLeaf
     , peanoCurve
-    , quadraticSnowflake
+    , pentaplexity
+    , quadraticGosper
+    , quadraticKochIsland1
+    , quadraticKochIsland2
+    , quadraticKochIsland3
+    , quadraticSnowflake1
+    , quadraticSnowflake2
+    , rings
+    , sierpinskiArrowhead
+    , snakeKolam
+    , squareSierpinski
+    , sticks
     , tiles
     , toTransformOptions
     , toTranslateOptions
     , tree
+    , triangle
     , vonKochSnowflake
+    , weed
     )
 
 import Data.Angle as Angle exposing (Angle)
@@ -172,68 +198,6 @@ leaf =
     }
 
 
-crystal : Settings
-crystal =
-    { default
-        | rules =
-            [ ( 'F', "FF+F++F+F" )
-            ]
-        , axiom = "F+F+F+F"
-        , iterations = 5
-        , lineLength = 5
-        , turningAngle = Angle.right
-        , windowPosition = { x = -140, y = -125 }
-        , windowSize = 1500
-    }
-
-
-peanoCurve : Settings
-peanoCurve =
-    { default
-        | rules =
-            [ ( 'X', "XFYFX+F+YFXFY-F-XFYFX" )
-            , ( 'Y', "YFXFY-F-XFYFX+F+YFXFY" )
-            ]
-        , axiom = "X"
-        , iterations = 4
-        , startHeading = Angle.right
-        , lineLength = 5
-        , turningAngle = Angle.right
-        , windowPosition = { x = -405, y = -5 }
-        , windowSize = 410
-    }
-
-
-quadraticSnowflake : Settings
-quadraticSnowflake =
-    { default
-        | rules =
-            [ ( 'F', "F-F+F+F-F" )
-            ]
-        , axiom = "F"
-        , iterations = 5
-        , lineLength = 3
-        , turningAngle = Angle.right
-        , windowPosition = { x = -10, y = -500 }
-        , windowSize = 750
-    }
-
-
-vonKochSnowflake : Settings
-vonKochSnowflake =
-    { default
-        | rules =
-            [ ( 'F', "F-F++F-F" )
-            ]
-        , axiom = "F++F++F"
-        , iterations = 4
-        , lineLength = 6
-        , turningAngle = Angle.fromDegrees 60
-        , windowPosition = { x = -60, y = -160 }
-        , windowSize = 600
-    }
-
-
 bush1 : Settings
 bush1 =
     { default
@@ -317,4 +281,527 @@ bush5 =
         , turningAngle = Angle.fromDegrees 40
         , windowPosition = { x = -13, y = -1 }
         , windowSize = 26
+    }
+
+
+sticks : Settings
+sticks =
+    { default
+        | rules =
+            [ ( 'F', "FF" )
+            , ( 'X', "F[+X]F[-X]+X" )
+            ]
+        , axiom = "X"
+        , iterations = 7
+        , lineLength = 1
+        , startHeading = Angle.right
+        , turningAngle = Angle.fromDegrees 20
+        , windowPosition = { x = -129, y = -5 }
+        , windowSize = 258
+    }
+
+
+algae1 : Settings
+algae1 =
+    { default
+        | rules =
+            [ ( 'a', "FFFFFv[+++h][---q]fb" )
+            , ( 'b', "FFFFFv[+++h][---q]fc" )
+            , ( 'c', "FFFFFv[+++fa]fd" )
+            , ( 'd', "FFFFFv[+++h][---q]fe" )
+            , ( 'e', "FFFFFv[+++h][---q]fg" )
+            , ( 'g', "FFFFFv[---fa]fa" )
+            , ( 'h', "ifFF" )
+            , ( 'i', "fFFF[--m]j" )
+            , ( 'j', "fFFF[--n]k" )
+            , ( 'k', "fFFF[--o]l" )
+            , ( 'l', "fFFF[--p]" )
+            , ( 'm', "fFn" )
+            , ( 'n', "fFo" )
+            , ( 'o', "fFp" )
+            , ( 'p', "fF" )
+            , ( 'q', "rfF" )
+            , ( 'r', "fFFF[++m]s" )
+            , ( 's', "fFFF[++n]t" )
+            , ( 't', "fFFF[++o]u" )
+            , ( 'u', "fFFF[++p]" )
+            , ( 'v', "Fv" )
+            ]
+        , axiom = "aF"
+        , iterations = 17
+        , lineLength = 1
+        , startHeading = Angle.right
+        , turningAngle = Angle.fromDegrees 12
+        , windowPosition = { x = -153, y = -3 }
+        , windowSize = 246
+    }
+
+
+algae2 : Settings
+algae2 =
+    --
+    -- N.B. This one doesn't look quite right. Check the reference image. What could be the issue?
+    --
+    { default
+        | rules =
+            [ ( 'a', "FFFFFy[++++n][----t]fb" )
+            , ( 'b', "+FFFFFy[++++n][----t]fc" )
+            , ( 'c', "FFFFFy[++++n][----t]fd" )
+            , ( 'd', "-FFFFFy[++++n][----t]fe" )
+            , ( 'e', "FFFFFy[++++n][----t]fg" )
+            , ( 'g', "FFFFFy[+++fa]fh" )
+            , ( 'h', "FFFFFy[++++n][----t]fi" )
+            , ( 'i', "+FFFFFy[++++n][----t]fj" )
+            , ( 'j', "FFFFFy[++++n][----t]fk" )
+            , ( 'k', "-FFFFFy[++++n][----t]fl" )
+            , ( 'l', "FFFFFy[++++n][----t]fm" )
+            , ( 'm', "FFFFFy[---fa]fa" )
+            , ( 'n', "ofFFF" )
+            , ( 'o', "fFFFp" )
+            , ( 'p', "fFFF[-s]q" )
+            , ( 'q', "fFFF[-s]r" )
+            , ( 'r', "fFFF[-s]" )
+            , ( 's', "fFfF" )
+            , ( 't', "ufFFF" )
+            , ( 'u', "fFFFv" )
+            , ( 'v', "fFFF[+s]w" )
+            , ( 'w', "fFFF[+s]x" )
+            , ( 'x', "fFFF[+s]" )
+            , ( 'y', "Fy" )
+            ]
+        , axiom = "aF"
+        , iterations = 17
+        , lineLength = 1
+        , startHeading = Angle.right
+        , turningAngle = Angle.fromDegrees 12
+        , windowPosition = { x = -153, y = -3 }
+        , windowSize = 246
+    }
+
+
+weed : Settings
+weed =
+    { default
+        | rules =
+            [ ( 'F', "FF-[XY]+[XY]" )
+            , ( 'X', "+FY" )
+            , ( 'Y', "-FX" )
+            ]
+        , axiom = "F"
+        , iterations = 6
+        , lineLength = 1
+        , startHeading = Angle.right
+        , turningAngle = Angle.fromDegrees 22.5
+        , windowPosition = { x = -63, y = -1 }
+        , windowSize = 126
+    }
+
+
+triangle : Settings
+triangle =
+    { default
+        | rules =
+            [ ( 'F', "F-F+F" )
+            ]
+        , axiom = "F+F+F"
+        , iterations = 7
+        , lineLength = 1
+        , startHeading = Angle.right
+        , turningAngle = Angle.fromDegrees 120
+        , windowPosition = { x = -34, y = -56 }
+        , windowSize = 62
+    }
+
+
+quadraticGosper : Settings
+quadraticGosper =
+    { default
+        | rules =
+            [ ( 'X', "XFX-YF-YF+FX+FX-YF-YFFX+YF+FXFXYF-FX+YF+FXFX+YF-FXYF-YF-FX+FX+YFYF-" )
+            , ( 'Y', "+FXFX-YF-YF+FX+FXYF+FX-YFYF-FX-YF+FXYFYF-FX-YFFX+FX+YF-YF-FX+FX+YFY" )
+            ]
+        , axiom = "-YF"
+        , iterations = 3
+        , lineLength = 1
+        , turningAngle = Angle.right
+        , windowPosition = { x = -1, y = -127 }
+        , windowSize = 128
+    }
+
+
+squareSierpinski : Settings
+squareSierpinski =
+    { default
+        | rules =
+            [ ( 'X', "XF-F+F-XF+F+XF-F+F-X" )
+            ]
+        , axiom = "F+XF+F+XF"
+        , iterations = 5
+        , lineLength = 1
+        , startHeading = Angle.right
+        , turningAngle = Angle.right
+        , windowPosition = { x = -126, y = -64 }
+        , windowSize = 128
+    }
+
+
+crystal : Settings
+crystal =
+    { default
+        | rules =
+            [ ( 'F', "FF+F++F+F" )
+            ]
+        , axiom = "F+F+F+F"
+        , iterations = 5
+        , lineLength = 5
+        , turningAngle = Angle.right
+        , windowPosition = { x = -140, y = -125 }
+        , windowSize = 1500
+    }
+
+
+peanoCurve : Settings
+peanoCurve =
+    { default
+        | rules =
+            [ ( 'X', "XFYFX+F+YFXFY-F-XFYFX" )
+            , ( 'Y', "YFXFY-F-XFYFX+F+YFXFY" )
+            ]
+        , axiom = "X"
+        , iterations = 4
+        , startHeading = Angle.right
+        , lineLength = 5
+        , turningAngle = Angle.right
+        , windowPosition = { x = -405, y = -5 }
+        , windowSize = 410
+    }
+
+
+quadraticSnowflake1 : Settings
+quadraticSnowflake1 =
+    { default
+        | rules =
+            [ ( 'F', "F-F+F+F-F" )
+            ]
+        , axiom = "F"
+        , iterations = 5
+        , lineLength = 3
+        , turningAngle = Angle.right
+        , windowPosition = { x = -10, y = -500 }
+        , windowSize = 750
+    }
+
+
+quadraticSnowflake2 : Settings
+quadraticSnowflake2 =
+    { default
+        | rules =
+            [ ( 'F', "F+F-F-F+F" )
+            ]
+        , axiom = "FF+FF+FF+FF"
+        , iterations = 4
+        , lineLength = 3
+        , turningAngle = Angle.right
+        , windowPosition = { x = -9, y = -10 }
+        , windowSize = 506
+    }
+
+
+quadraticKochIsland1 : Settings
+quadraticKochIsland1 =
+    { default
+        | rules =
+            [ ( 'F', "F+F-F-FFF+F+F-F" )
+            ]
+        , axiom = "F+F+F+F"
+        , iterations = 3
+        , lineLength = 3
+        , turningAngle = Angle.right
+        , windowPosition = { x = -36, y = -178 }
+        , windowSize = 370
+    }
+
+
+quadraticKochIsland2 : Settings
+quadraticKochIsland2 =
+    { default
+        | rules =
+            [ ( 'F', "F-FF+FF+F+F-F-FF+F+F-F-FF-FF+F" )
+            ]
+        , axiom = "F+F+F+F"
+        , iterations = 2
+        , lineLength = 3
+        , turningAngle = Angle.right
+        , windowPosition = { x = -43, y = -43 }
+        , windowSize = 194
+    }
+
+
+quadraticKochIsland3 : Settings
+quadraticKochIsland3 =
+    { default
+        | rules =
+            [ ( 'X', "X+YF++YF-FX--FXFX-YF+X" )
+            , ( 'Y', "-FX+YFYF++YF+FX--FX-YF" )
+            ]
+        , axiom = "X+X+X+X+X+X+X+X"
+        , iterations = 3
+        , lineLength = 0.5
+        , turningAngle = Angle.fromDegrees 45
+        , windowPosition = { x = -117, y = -22 }
+        , windowSize = 140
+    }
+
+
+board : Settings
+board =
+    { default
+        | rules =
+            [ ( 'F', "FF+F+F+F+FF" )
+            ]
+        , axiom = "F+F+F+F"
+        , iterations = 4
+        , lineLength = 1
+        , turningAngle = Angle.right
+        , windowPosition = { x = -7, y = -7 }
+        , windowSize = 94
+    }
+
+
+hilbert : Settings
+hilbert =
+    { default
+        | rules =
+            [ ( 'X', "-YF+XFX+FY-" )
+            , ( 'Y', "+XF-YFY-FX+" )
+            ]
+        , axiom = "X"
+        , iterations = 7
+        , lineLength = 1
+        , turningAngle = Angle.right
+        , windowPosition = { x = -4, y = -131 }
+        , windowSize = 135
+    }
+
+
+sierpinskiArrowhead : Settings
+sierpinskiArrowhead =
+    { default
+        | rules =
+            [ ( 'X', "YF+XF+Y" )
+            , ( 'Y', "XF-YF-X" )
+            ]
+        , axiom = "YF"
+        , iterations = 7
+        , lineLength = 1
+        , turningAngle = Angle.fromDegrees 60
+        , windowPosition = { x = -4, y = -121 }
+        , windowSize = 135
+    }
+
+
+vonKochSnowflake : Settings
+vonKochSnowflake =
+    { default
+        | rules =
+            [ ( 'F', "F-F++F-F" )
+            ]
+        , axiom = "F++F++F"
+        , iterations = 4
+        , lineLength = 6
+        , turningAngle = Angle.fromDegrees 60
+        , windowPosition = { x = -60, y = -160 }
+        , windowSize = 600
+    }
+
+
+cross1 : Settings
+cross1 =
+    { default
+        | rules =
+            [ ( 'F', "F+FF++F+F" )
+            ]
+        , axiom = "F+F+F+F"
+        , iterations = 5
+        , lineLength = 1
+        , turningAngle = Angle.right
+        , windowPosition = { x = -84, y = -43 }
+        , windowSize = 89
+    }
+
+
+cross2 : Settings
+cross2 =
+    { default
+        | rules =
+            [ ( 'F', "F+F-F+F+F" )
+            ]
+        , axiom = "F+F+F+F"
+        , iterations = 5
+        , lineLength = 1
+        , turningAngle = Angle.right
+        , windowPosition = { x = -7, y = -32 }
+        , windowSize = 89
+    }
+
+
+pentaplexity : Settings
+pentaplexity =
+    { default
+        | rules =
+            [ ( 'F', "F++F++F|F-F++F" )
+            ]
+        , axiom = "F++F++F++F++F"
+        , iterations = 4
+        , lineLength = 1
+        , turningAngle = Angle.fromDegrees 36
+        , windowPosition = { x = -15, y = -3 }
+        , windowSize = 77
+    }
+
+
+rings : Settings
+rings =
+    { default
+        | rules =
+            [ ( 'F', "FF+F+F+F+F+F-F" )
+            ]
+        , axiom = "F+F+F+F"
+        , iterations = 4
+        , lineLength = 1
+        , turningAngle = Angle.right
+        , windowPosition = { x = -106, y = -10 }
+        , windowSize = 144
+    }
+
+
+dragonCurve : Settings
+dragonCurve =
+    { default
+        | rules =
+            [ ( 'X', "X+YF+" )
+            , ( 'Y', "-FX-Y" )
+            ]
+        , axiom = "FX"
+        , iterations = 12
+        , lineLength = 1
+        , turningAngle = Angle.right
+        , windowPosition = { x = -75, y = -36 }
+        , windowSize = 98
+    }
+
+
+hexagonalGosper : Settings
+hexagonalGosper =
+    { default
+        | rules =
+            [ ( 'X', "X+YF++YF-FX--FXFX-YF+" )
+            , ( 'Y', "-FX+YFYF++YF+FX--FX-Y" )
+            ]
+        , axiom = "XF"
+        , iterations = 4
+        , lineLength = 1
+        , turningAngle = Angle.fromDegrees 60
+        , windowPosition = { x = -38, y = -2 }
+        , windowSize = 58
+    }
+
+
+levyCurve : Settings
+levyCurve =
+    { default
+        | rules =
+            [ ( 'F', "-F++F-" )
+            ]
+        , axiom = "F"
+        , iterations = 12
+        , lineLength = 1
+        , turningAngle = Angle.fromDegrees 45
+        , windowPosition = { x = -33, y = -85 }
+        , windowSize = 130
+    }
+
+
+classicSierpinskiCurve : Settings
+classicSierpinskiCurve =
+    { default
+        | rules =
+            [ ( 'X', "XF+F+XF--F--XF+F+X" )
+            ]
+        , axiom = "XF+F+XF--F--XF+F+X"
+        , iterations = 4
+        , lineLength = 1
+        , turningAngle = Angle.fromDegrees 45
+        , windowPosition = { x = -2, y = -30 }
+        , windowSize = 110
+    }
+
+
+krishnaAnklets : Settings
+krishnaAnklets =
+    { default
+        | rules =
+            [ ( 'X', "XFX--XFX" )
+            ]
+        , axiom = "-X--X"
+        , iterations = 6
+        , lineLength = 1
+        , turningAngle = Angle.fromDegrees 45
+        , windowPosition = { x = -46, y = -91 }
+        , windowSize = 92
+    }
+
+
+mangoLeaf : Settings
+mangoLeaf =
+    --
+    -- TODO: Implement openPolygon and closePolygon.
+    --
+    { default
+        | rules =
+            [ ( 'X', "{F-F}{F-F}--[--X]{F-F}{F-F}--{F-F}{F-F}--" )
+            , ( 'Y', "f-F+X+F-fY" )
+            ]
+        , axiom = "Y---Y"
+        , iterations = 1
+        , lineLength = 1
+        , turningAngle = Angle.fromDegrees 60
+        , windowPosition = { x = -2, y = -3 }
+        , windowSize = 7
+    }
+
+
+snakeKolam : Settings
+snakeKolam =
+    --
+    -- TODO: Implement openPolygon and closePolygon.
+    --
+    { default
+        | rules =
+            [ ( 'X', "X{F-F-F}+XF+F+X{F-F-F}+X" )
+            ]
+        , axiom = "F+XF+F+XF"
+        , iterations = 1
+        , lineLength = 1
+        , turningAngle = Angle.right
+        , windowPosition = { x = -60, y = -160 }
+        , windowSize = 600
+    }
+
+
+kolam : Settings
+kolam =
+    { default
+        | rules =
+            [ ( 'A', "F++FFFF--F--FFFF++F++FFFF--F" )
+            , ( 'B', "F--FFFF++F++FFFF--F--FFFF++F" )
+            , ( 'C', "BFA--BFA" )
+            , ( 'D', "CFC--CFC" )
+            ]
+        , axiom = "(-D--D)"
+        , iterations = 3
+        , lineLength = 1
+        , turningAngle = Angle.fromDegrees 45
+        , windowPosition = { x = -15, y = -28 }
+        , windowSize = 30
     }
